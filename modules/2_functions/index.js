9a09913d7e0498b96cbbe3586c79bc4a7c16d44f
Hevanddd <mandzik123@gmail.com>
2020-11-02 23:51:11 +0200

    Done second module

--- modules/2_functions/index.js
+++ modules/2_functions/index.js
@@ -1,27 +1,80 @@
 /**
  * Currying 
  */
-export const mergeWords = () => {};
+export const mergeWords = (word) => {
+  return (nextWord) => {
+    if (nextWord === undefined) {
+      return word;
+    } else {
+      return mergeWords(word + ' ' + nextWord);
+    }
+  }
+};
 
 /**
  * Every/Some
  */
-export const checkUsersValid = () => {};
+export const checkUsersValid = (goodUsers) => {
+  return function allUsersValid(submittedUsers) {
+    return submittedUsers.every(function (submittedUser) {
+      return goodUsers.some(function (goodUser) {
+        return goodUser.id === submittedUser.id;
+      });
+    });
+  };
+};
 
 /**
  * Reduce 
  */
-export const countWords = () => {};
+export const countWords = (words) => {
+  return words.reduce((resultObj, word) => {
+    resultObj[word] = ++resultObj[word] || 1;
+    return resultObj;
+  }, {});
+};
 
 /**
  * Palindrome 
  */
-export const isPalindrome = () => {};
+export const isPalindrome = (str) => {
+  return str == str.split('').reverse().join('');
+};
 
 /**
  * Recursion 
  */
-export const factorial = () => {};
-export const amountToCoins = () => {};
-export const repeat = () => {};
-export const reduce = () => {};
+export const factorial = (n) => {
+  if ((n === 0) || (n === 1))
+    return 1;
+  else
+    return (n * factorial(n - 1));
+};
+
+export const amountToCoins = (amount, coins) => {
+  if (amount === 0) {
+    return [];
+  } else {
+    if (amount >= coins[0]) {
+      left = (amount - coins[0]);
+      return [coins[0]].concat(amountTocoins(left, coins));
+    } else {
+      coins.shift();
+      return amountTocoins(amount, coins);
+    }
+  }
+};
+
+export const repeat = (func, n) => {
+  func();
+  if (n > 1) {
+    repeat(func, n - 1);
+  }
+};
+
+export const reduce = (iterable, reduceFn, accumulator) => {
+  for (let i of iterable) {
+    accumulator = reduceFn(accumulator, i)
+  }
+  return accumulator
+};
